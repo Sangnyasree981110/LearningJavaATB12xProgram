4f404d1b1ddb5f819b82dfc1e759bcc67c8b60b9
biswa <sangnyasreeb@gmail.com>
2025-05-18 13:07:47 +0530

    added

---
+++ src/ex_01_Java_Basic/Lab003_Comments.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Comments {
+    public static void main(String[] args) {
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        System.out.println("Hellos World!");
+    }
+
+}
