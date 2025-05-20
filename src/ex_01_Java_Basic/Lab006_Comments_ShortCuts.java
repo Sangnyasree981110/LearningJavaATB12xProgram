4b94518bc254e2543d1e7b70fc2757352e83d163
biswa <sangnyasreeb@gmail.com>
2025-05-20 22:20:42 +0530

    added

---
+++ src/ex_01_Java_Basic/Lab006_Comments_ShortCuts.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Comments_ShortCuts {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+    }
+}
---
+++ src/ex_01_Java_Basic/Multiple_Main.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Multiple_Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+}
