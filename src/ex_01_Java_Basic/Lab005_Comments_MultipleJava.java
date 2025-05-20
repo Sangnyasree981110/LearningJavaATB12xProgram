f2fb413a8b255a09a83405fb24b9dc60df84eeff
biswa <sangnyasreeb@gmail.com>
2025-05-20 22:20:42 +0530

    added

---
+++ src/ex_01_Java_Basic/Lab005_Comments_MultipleJava.java
@@ -0,0 +1,20 @@
+package ex_01_Java_Basic;
+/**
+ * Author : Pramod Dutta
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
+
+public class Lab005_Comments_MultipleJava{
+    /**
+     * Author : Pramod Dutta
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+    public static void main(String[] args) {
+
+        System.out.println("Multi Comment");
+    }
+}
